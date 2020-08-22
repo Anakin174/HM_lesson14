dd1096135ef5ef3a85079772f6a1e8585e4dda9c
Anakin174 <anakin174jedi@gmail.com>
2020-08-22 19:05:49 +0300

    add build and prod instance

--- config.tf
+++ config.tf
@@ -6,22 +6,42 @@ variable "image_id" {
   default = "ami-0bbe28eb2173f6167"
 }
 
-variable "vpc_id" {
-  default = "vpc-ea13c881"
+variable "security_group" {
+  default = "sg-09ce11f213f8c6b3c"
 }
 
 resource "aws_instance" "build_instance" {
   ami = "${var.image_id}"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.amazon.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.Open.id}"]
+  vpc_security_group_ids = "${var.security_group}"
   subnet_id = "${var.subnet_id}"
   user_data = <<EOF
 #!/bin/bash
 sudo apt update && sudo apt install -y maven awscli
 git clone https://github.com/Anakin174/boxfuse.git
 cd boxfuse && mvn clean package
-
+export AWS_ACCESS_KEY_ID=<...placeholder...>
+export AWS_SECRET_ACCESS_KEY=<...placeholder...>
+export AWS_DEFAULT_REGION=us-east-1
+aws s3 cp target/hello-1.0.war s3://boxfuse-test-web
 EOF
+}
 
+resource "aws_instance" "prod_instance" {
+  ami = "${var.image_id}"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.amazon.key_name}"
+  vpc_security_group_ids = "${var.security_group}"
+  subnet_id = "${var.subnet_id}"
+  user_data = <<EOF
+#!/bin/bash
+sudo apt update && sudo apt install -y openjdk-8-jdk tomcat8 awscli
+export AWS_ACCESS_KEY_ID=<...placeholder...>
+export AWS_SECRET_ACCESS_KEY=<...placeholder...>
+export AWS_DEFAULT_REGION=us-east-1
+aws s3 cp s3://boxfuse-test-web/hello-1.0.war /tmp/hello-1.0.war
+sudo mv /tmp/hello-1.0.war /var/lib/tomcat8/webapps/hello-1.0.war
+sudo systemctl restart tomcat8
+EOF
 }
