92d0a5441831dba166c2648ab8fd188f8bf0f8ff
Anakin174 <anakin174jedi@gmail.com>
2020-08-23 19:42:52 +0300

    debug path

--- config.tf
+++ config.tf
@@ -16,8 +16,8 @@ variable "subnet_id" {
 }
 
 variable "ssh-key" {
- # default = "/home/ubuntu/my-key.pem"
-  default = "file("~/.ssh/id_rsa")}"
+  default = "~/.ssh/id_rsa"
+ # default = "file("~/.ssh/id_rsa")}"
 }
 
 
