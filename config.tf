6cbffb85d6f4cd753d4b1b2544df07e34e7984b0
Anakin174 <anakin174jedi@gmail.com>
2020-08-23 20:26:14 +0300

    fix

--- config.tf
+++ config.tf
@@ -39,8 +39,8 @@ EOF
 resource "aws_s3_bucket_object" "war" {
   bucket = "boxfuse-test-web"
   key = "hello.war"
-  source = "${file("${aws_instance.build}/home/boxfuse/target/hello-1.0.war")}"
-  etag = "${md5(file("${aws_instance.build}/home/boxfuse/target/hello-1.0.war"))}"
+  source = "${file("${aws_instance.build_instance}/home/boxfuse/target/hello-1.0.war")}"
+  etag = "${md5(file("${aws_instance.build_instance}/home/boxfuse/target/hello-1.0.war"))}"
 }
 
 
