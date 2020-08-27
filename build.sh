a248b1facd7d7bd2c6a2f1fdab06412dc668d260
Anakin174 <anakin174jedi@gmail.com>
2020-08-27 20:18:24 +0300

    change username github

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt install -y maven awscli
-git clone https://github.com/Anakin174/boxfuse.git
+git clone https://github.com/v-kamerdinerov/boxfuse.git
 cd boxfuse && mvn clean package
 cd ~
 mkdir .aws && cd .aws
